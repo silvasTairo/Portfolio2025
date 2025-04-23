b8d7dcfade13138019d9f219a490867339b56cc8
TairoSilva <162700891+silvasTairo@users.noreply.github.com>
2025-04-23 20:18:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.sidebar .nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // Previne o comportamento padrão do link
+
+            const targetId = link.getAttribute('href').substring(1); // Obtém o ID do destino
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                // Animação personalizada usando scrollIntoView
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+});
+
+document.getElementById("linguagens").onclick = function() {
+    window.location.href = "linguagens-trimestres.html";
+};
+document.getElementById("humanas").onclick = function() {
+    window.location.href = "humanas-trimestres.html";
+};
+document.getElementById("matematica").onclick = function() {
+    window.location.href = "matematica-trimestres.html";
+};
+document.getElementById("natureza").onclick = function() {
+    window.location.href = "natureza-trimestres.html";
+};
+
+
+
+document.getElementById("modelagem").onclick = function() {
+    window.location.href = "modelagem.html";
+};
+document.getElementById("desenvolvimento").onclick = function() {
+    window.location.href = "desenvolvimento.html";
+};
+document.getElementById("iot").onclick = function() {
+    window.location.href = "iot.html";
+};
---
+++ materias.js
@@ -0,0 +1,5 @@
+
+document.getElementById("1°trimestre").addEventListener("click", function() {
+    const div = document.getElementById("atividades");
+    div.classList.toggle("ativo"); // Adiciona ou remove a classe "ativo"
+});
