516b63be6a163f292e605c1f9a2f3abd6536fb9d
silvasTairo <tairo_silva@estudante.sesisenai.org.br>
2025-08-08 12:43:28 -0300

    update linguagens

--- materias.js
+++ materias.js
@@ -3,3 +3,13 @@ document.getElementById("1°trimestre").addEventListener("click", function() {
     const div = document.getElementById("atividades");
     div.classList.toggle("ativo"); // Adiciona ou remove a classe "ativo"
 });
+
+document.getElementById("2°trimestre").addEventListener("click", function() {
+    const div = document.getElementById("atividades2");
+    div.classList.toggle("ativo"); // Adiciona ou remove a classe "ativo"
+});
+
+document.getElementById("3°trimestre").addEventListener("click", function() {
+    const div = document.getElementById("atividades3");
+    div.classList.toggle("ativo"); // Adiciona ou remove a classe "ativo"
+});
